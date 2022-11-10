0ae28ee06c82ed18754325f913053eb659fcfe0c
Michael Clark <michael.clark@deswik.com>
2022-11-11 07:16:24 +1000

    Refactor solved words output. Add some new statistics.

--- Lookup/Program.cs
+++ Lookup/Program.cs
@@ -17,14 +17,22 @@ var lookupNames = new[]
 var solver = new Trie(dictionaryPath);
 var solvedDictionary = solver.SolveRange(lookupNames);
 
-foreach (var (lookup, words) in solvedDictionary.OrderBy(sd => sd.Value.Length))
+foreach (var (name, matchWords) in solvedDictionary.OrderBy(sd => sd.Value.Length))
 {
-    
-    Console.WriteLine($"{lookup} ({words.Length})");
-    var wordsOrderedByLength = words.OrderByDescending(w => w.Length);
-    var top25Words = wordsOrderedByLength.Take(25);
-    foreach (var top25Word in top25Words)
+    var matchWordCount = matchWords.Length;
+    var matchWordsPerLetter = matchWordCount / (double)name.Length;
+
+    var longestWordLength = matchWords.Max(w => w.Length);
+    var longestWords = matchWords.Where(w => w.Length == longestWordLength);
+
+    var topWords = matchWords.OrderByDescending(w => w.Length).Take(TopWordsToShow);
+
+    var sb = new StringBuilder();
+    sb.AppendLine($"{name} ({matchWordCount} @ {matchWordsPerLetter:N2}) [{longestWordLength} - {string.Join(", ", longestWords)}]");
+    foreach (var topWord in topWords)
     {
-        Console.WriteLine($"\t{top25Word}");
+        sb.AppendLine($"\t{topWord}");
     }
+
+    Console.WriteLine(sb.ToString());
 }
